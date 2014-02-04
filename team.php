d1c8a73a828e7e4eb25b0ac8cb061dd1b1d08f8a
Hugo <hcastanho@gmail.com>
2014-02-04 20:08:41 +0000

    Mentor

--- team.php
+++ team.php
@@ -8,14 +8,15 @@
 					<article>
 						<h1>Mentor e Equipa</h1>
 						<div class="sharethis">
-							<span class='st_facebook_hcount'></span>
-							<span class='st_twitter_hcount'></span>
-							<span class='st_sharethis_hcount'></span>
+							<span class='st_facebook'></span>
+							<span class='st_twitter'></span>
+							<span class='st_instagram'></span>
 						</div>
 						<cite>
 							“Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas“							
 						</cite>
 						<div class="content">
+							<p>Etiam tempor auctor dolor, in viverra neque lacinia sed. Nulla ac interdum enim. Nulla facilisi. Nulla rutrum semper enim, sed consequat turpis dictum vitae. Nulla facilisi. In hac habitasse platea dictumst. </p>							
 							<p>Etiam tempor auctor dolor, in viverra neque lacinia sed. Nulla ac interdum enim. Nulla facilisi. Nulla rutrum semper enim, sed consequat turpis dictum vitae. Nulla facilisi. In hac habitasse platea dictumst. </p>							
 						</div>
 						<a href="" class="button">Lê o perfil completo »</a>
@@ -41,80 +42,67 @@
 		</div><!-- #top-stories + + + + + + + + + + + + + + + + + + -->
 		
 
-		<div id="videos">
+		<div id="team-members">
+			<div class="row">
+				<section class="profile">
+					<header>Equipa</header>
+					<ul class="listing-7">
+						<li>
+							<a href="">
+								<img src="assets/img/placeholder.jpg" alt="">
+								<h2>Automatically generated caption</h2>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<img src="assets/img/placeholder.jpg" alt="">
+								<h2>Automatically generated caption</h2>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<img src="assets/img/placeholder.jpg" alt="">
+								<h2>Automatically generated caption</h2>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<img src="assets/img/placeholder.jpg" alt="">
+								<h2>Automatically generated caption</h2>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<img src="assets/img/placeholder.jpg" alt="">
+								<h2>Automatically generated caption</h2>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<img src="assets/img/placeholder.jpg" alt="">
+								<h2>Automatically generated caption</h2>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<img src="assets/img/placeholder.jpg" alt="">
+								<h2>Automatically generated caption</h2>
+							</a>
+						</li>
+					</ul>
+				</section>
+			</div><!-- .row -->
+		</div><!-- #team + + + + + + + + + + + + + + + + + + -->
+
+
+		<div id="team-videos">
 			<div class="row">
 				<section class="videos">
-					<header>Últimos Vídeos <a href="">+ videos</a></header>
+					<header>Vídeos</header>
 					<ul class="listing-4">
 						<li>
 							<a href="">
 								<img src="assets/img/placeholder.jpg" alt="">
-								<h2>Automatically generated caption</h2>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h2>Automatically generated caption</h2>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h2>Automatically generated caption</h2>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h2>Automatically generated caption</h2>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h2>Automatically generated caption</h2>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h2>Automatically generated caption</h2>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h2>Automatically generated caption</h2>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h2>Automatically generated caption</h2>
-							</a>
-						</li>
-					</ul>
-				</section>
-			</div><!-- .row -->
-		</div><!-- #videos + + + + + + + + + + + + + + + + + + -->
-
-
-
-		<div id="pictures">
-			<div class="row">
-				<section class="latest">
-					<header>Últimos <a href="">+ artigos</a></header>
-					<ul class="vertical-2">
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
 								<h3>Automatically generated caption</h3>
 							</a>
 						</li>
@@ -138,76 +126,15 @@
 						</li>
 					</ul>
 				</section>
-
-				<section class="images">
-					<header>Galerias <a href="">+ galerias</a></header>
-					<ul class="listing-3">
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>											
-					</ul>					
-				</section>
 			</div><!-- .row -->
-		</div><!-- #pictures + + + + + + + + + + + + + + + + + + -->
+		</div><!-- #team-videos + + + + + + + + + + + + + + + + + + -->
 
 
-
-		<div id="reporter">
+		<div id="team-gallery">
 			<div class="row">
-				<section class="reporter">
-					<header>Repórter V <a href="">+ artigos</a></header>
-					<ul class="listing-2-hz">
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
+				<section class="gallery">
+					<header>Galeria</header>
+					<ul class="listing-4">
 						<li>
 							<a href="">
 								<img src="assets/img/placeholder.jpg" alt="">
@@ -234,31 +161,9 @@
 						</li>					
 					</ul>					
 				</section>
-				<section class="a3">
-					<header>Antena 3 <a href="">+ entradas</a></header>
-					<ul class="vertical-2">
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>
-						<li>
-							<a href="">
-								<img src="assets/img/placeholder.jpg" alt="">
-								<h3>Automatically generated caption</h3>
-							</a>
-						</li>				
-					</ul>					
-				</section>
 			</div><!-- .row -->
-		</div><!-- #pictures + + + + + + + + + + + + + + + + + + -->
+		</div><!-- #team-gallery + + + + + + + + + + + + + + + + + + -->
+
 
 		<?php require_once("template_parts/footer-mentors.php"); ?>
 
