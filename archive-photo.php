c66161b6278c447c253eb382d33d0dda4ce401d8
Hugo <hcastanho@gmail.com>
2014-02-06 18:52:50 +0000

    Added Galleries and Archives

---
+++ archive-photo.php
@@ -0,0 +1,205 @@
+<?php require_once("template_parts/header.php"); ?>
+
+		<?php require_once("template_parts/site-header.php"); ?>
+
+		<div id="top-stories">
+			<div class="row">
+
+				<div class="primary">	
+
+					<section id="archive" class="photo">
+						<header>Galerias</header>
+						<ul class="">
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>			
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>			
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>			
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>	
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>			
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>	
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>			
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>	
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>			
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>	
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>			
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>	
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>			
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>	
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>			
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>	
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>			
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h3>Automatically generated caption</h3>
+								</a>
+							</li>	
+						</ul>
+					</section><!-- #profile-mentor -->
+
+
+					<ul class="pagination">
+					    <li><a href="#"> < </a></li>
+					    <li class="on"><a href="#">1</a></li>
+					    <li><a href="#">2</a></li>
+					    <li><a href="#">3</a></li>
+					    <li><a href="#">4</a></li>
+					    <li><a href="#"> > </a></li>
+					</ul>
+
+
+				</div><!-- .primary -->
+
+				<aside>
+					<?php require_once("template_parts/aside-first.php"); ?>					
+					<?php require_once("template_parts/aside-second.php"); ?>
+				</aside>
+
+			</div><!-- .row -->
+		</div><!-- #top-stories + + + + + + + + + + + + + + + + + + -->
+
+		<?php require_once("template_parts/footer-mentors.php"); ?>
+
+		<?php require_once("template_parts/footer-hosts.php"); ?>
+
+		<?php require_once("template_parts/footer-rtpplay.php"); ?>
+
+		<?php require_once("template_parts/footer.php"); ?>
---
+++ archive.php
@@ -0,0 +1,113 @@
+<?php require_once("template_parts/header.php"); ?>
+
+		<?php require_once("template_parts/site-header.php"); ?>
+
+		<div id="top-stories">
+			<div class="row">
+
+				<div class="primary">	
+
+					<section id="archive" class="no-header">
+						<header><img src="assets/img/placeholder1.png" alt=""></header>
+						<ul class="">
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h2>Automatically generated caption</h2>
+								</a>
+								<p>Praesent elementum cursus elit, ut consequat enim pulvinar eget. Sed id nunc sodales, iaculis nisi in, venenatis est. Vestibulum consectetur est dapibus, tempus ante non, ultricies nulla.</p>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h2>Automatically generated caption</h2>
+								</a>
+								<p>Praesent elementum cursus elit, ut consequat enim pulvinar eget. Sed id nunc sodales, iaculis nisi in, venenatis est. Vestibulum consectetur est dapibus, tempus ante non, ultricies nulla.</p>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h2>Automatically generated caption</h2>
+								</a>
+								<p>Praesent elementum cursus elit, ut consequat enim pulvinar eget. Sed id nunc sodales, iaculis nisi in, venenatis est. Vestibulum consectetur est dapibus, tempus ante non, ultricies nulla.</p>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h2>Automatically generated caption</h2>
+								</a>
+								<p>Praesent elementum cursus elit, ut consequat enim pulvinar eget. Sed id nunc sodales, iaculis nisi in, venenatis est. Vestibulum consectetur est dapibus, tempus ante non, ultricies nulla.</p>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h2>Automatically generated caption</h2>
+								</a>
+								<p>Praesent elementum cursus elit, ut consequat enim pulvinar eget. Sed id nunc sodales, iaculis nisi in, venenatis est. Vestibulum consectetur est dapibus, tempus ante non, ultricies nulla.</p>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h2>Automatically generated caption</h2>
+								</a>
+								<p>Praesent elementum cursus elit, ut consequat enim pulvinar eget. Sed id nunc sodales, iaculis nisi in, venenatis est. Vestibulum consectetur est dapibus, tempus ante non, ultricies nulla.</p>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h2>Automatically generated caption</h2>
+								</a>
+								<p>Praesent elementum cursus elit, ut consequat enim pulvinar eget. Sed id nunc sodales, iaculis nisi in, venenatis est. Vestibulum consectetur est dapibus, tempus ante non, ultricies nulla.</p>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h2>Automatically generated caption</h2>
+								</a>
+								<p>Praesent elementum cursus elit, ut consequat enim pulvinar eget. Sed id nunc sodales, iaculis nisi in, venenatis est. Vestibulum consectetur est dapibus, tempus ante non, ultricies nulla.</p>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h2>Automatically generated caption</h2>
+								</a>
+								<p>Praesent elementum cursus elit, ut consequat enim pulvinar eget. Sed id nunc sodales, iaculis nisi in, venenatis est. Vestibulum consectetur est dapibus, tempus ante non, ultricies nulla.</p>
+							</li>
+							<li>
+								<a href="">
+									<img src="assets/img/placeholder.jpg" alt="">
+									<h2>Automatically generated caption</h2>
+								</a>
+								<p>Praesent elementum cursus elit, ut consequat enim pulvinar eget. Sed id nunc sodales, iaculis nisi in, venenatis est. Vestibulum consectetur est dapibus, tempus ante non, ultricies nulla.</p>
+							</li>
+						</ul>
+					</section><!-- #profile-mentor -->
+
+
+					<ul class="pagination">
+					    <li><a href="#"> < </a></li>
+					    <li class="on"><a href="#">1</a></li>
+					    <li><a href="#">2</a></li>
+					    <li><a href="#">3</a></li>
+					    <li><a href="#">4</a></li>
+					    <li><a href="#"> > </a></li>
+					</ul>
+
+
+				</div><!-- .primary -->
+
+				<aside>
+					<?php require_once("template_parts/aside-first.php"); ?>					
+					<?php require_once("template_parts/aside-second.php"); ?>
+				</aside>
+
+			</div><!-- .row -->
+		</div><!-- #top-stories + + + + + + + + + + + + + + + + + + -->
+
+		<?php require_once("template_parts/footer-mentors.php"); ?>
+
+		<?php require_once("template_parts/footer-hosts.php"); ?>
+
+		<?php require_once("template_parts/footer-rtpplay.php"); ?>
+
+		<?php require_once("template_parts/footer.php"); ?>
